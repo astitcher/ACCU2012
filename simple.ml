b7be7ffc09f072506c361541a111429675373acc
Andrew Stitcher <astitcher@apache.org>
2012-03-22 02:11:20 -0400

    Refactored to put all the information about the operators in one place

--- simple.ml
+++ simple.ml
@@ -9,10 +9,41 @@ type binop =
   | Divide
   | Power
 
+let asfloat v =
+  match v with
+      Int i -> (float i)
+    | Float f -> f
+
+let lookup_binop =
+  let dobinop iop fop v1 v2 =
+    match (v1, v2) with
+        (Int i, Int j) -> Int (iop i j)
+      | _  -> Float ( fop (asfloat v1) (asfloat v2))
+  in
+  function
+    | Plus -> "+",0, dobinop (+) (+.)
+    | Minus -> "-",0, dobinop (-) (-.)
+    | Multiply -> "*",2, dobinop ( * ) ( *. )
+    | Divide -> "/",2, dobinop (/) (/.)
+    | Power -> "**",4, 
+        (function x -> function y -> Float( (asfloat x)**(asfloat y)) )
+
 type unop =
     Negate
   | Square
   | Sqroot
+
+let lookup_unop =
+  let dounop iop fop v = match v with
+      Int i -> Int (iop i)
+    | Float f -> Float (fop f)
+  in
+  let thingyop op v = op v v
+  in
+  function
+    | Negate -> "~-", dounop (~-) (~-.)
+    | Square -> "sqr", dounop (thingyop ( * )) (thingyop ( *. ))
+    | Sqroot -> "sqroot", function v -> Float (sqrt (asfloat v))
 
 type expression =
     BinOp of binop * expression * expression
@@ -20,40 +51,13 @@ type expression =
   | Value of value
 
 let rec eval e =
-  let asfloat v =
-    match v with
-        Int i -> (float i)
-      | Float f -> f
-  in
   let evalbinop op v1 v2 =
-    let dobinop iop fop v1 v2 =
-      match (v1, v2) with
-          (Int i, Int j) -> Int (iop i j)
-        | _  -> Float ( fop (asfloat v1) (asfloat v2))
-    in
-    let dofunc =
-      match op with
-          Plus -> dobinop (+) (+.)
-        | Minus -> dobinop (-) (-.)
-        | Multiply -> dobinop ( * ) ( *. )
-        | Divide -> dobinop (/) (/.)
-        | Power -> 
-            (function x -> function y -> Float( (asfloat x)**(asfloat y)) )
+    let _, _, dofunc = lookup_binop op
     in
     dofunc v1 v2
   in
   let evalunop op v =
-    let dounop iop fop v = match v with
-        Int i -> Int (iop i)
-      | Float f -> Float (fop f)
-    in
-    let thingyop op v = op v v
-    in
-    let dofunc =
-      match op with
-          Negate -> dounop (~-) (~-.)
-        | Square -> dounop (thingyop ( * )) (thingyop ( *. ))
-        | Sqroot -> function v -> Float (sqrt (asfloat v))
+    let _, dofunc = lookup_unop op
     in
     dofunc v
   in
@@ -70,23 +74,13 @@ let string_of_exp e =
       else s
     in
     let string_of_binop op e1 e2 p =
-      let s, pred =
-        match op with
-            Plus -> "+",0
-          | Minus -> "-",0
-          | Multiply -> "*",2
-          | Divide -> "/",2
-          | Power -> "**",4
+      let s, pred, _ = lookup_binop op
       in
       dobraces pred p
         ((string_of_exp_b e1 pred)^s^(string_of_exp_b e2 pred))
     in
     let string_of_unop op e p =
-      let s =
-        match op with
-            Negate -> "~-"
-          | Square -> "sqr"
-          | Sqroot -> "sqroot"
+      let s, _ = lookup_unop op
       in
       s^"("^(string_of_exp_b e p)^")"
     in
