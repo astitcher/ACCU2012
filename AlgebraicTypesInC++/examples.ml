6e6213c351246bd6cfa64ac68d1167180b808ec1
Andrew Stitcher <astitcher@redhat.com>
2012-04-23 18:05:38 -0400

    Added sml example for simple number model

--- AlgebraicTypesInC++/examples.ml
+++ AlgebraicTypesInC++/examples.ml
@@ -8,4 +8,19 @@ type complex = float * float
 
 type rational = int * int
 (* ]]]]SimpleRecord *)
+(* [[[[SmlNumberType *)
+type number =
+    | Int of int
+    | Float of float
+    | Rational of rational
+    | Complex of complex
+(* ]]]]SmlNumberType *)
+(* [[[[SmlNumberMag *)
+let magnitude n =
+    match n with
+    | Int i -> abs_float (float i)
+    | Float f -> abs_float f
+    | Complex (r,i) -> sqrt(r**2. +. i**2.)
+    | Rational (a,b) -> abs_float (float a /. float b)
+(* ]]]]SmlNumberMag *)
 
