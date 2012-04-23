2f7156a09c54ca4e86cf161a1116b12179f9fc68
Andrew Stitcher <astitcher@redhat.com>
2012-04-23 15:17:07 -0400

    Added code segment notations - added more efficient accumlating version of length

--- AlgebraicTypesInC++/intlist.ml
+++ AlgebraicTypesInC++/intlist.ml
@@ -1,11 +1,23 @@
+(* [[[[SmlIntlist *)
 type intlist = Cons of int * intlist | Nil
 
 let rec length il = 
   match il with
     | Nil -> 0
     | Cons (_, il) -> 1 + length(il)
+(* ]]]]SmlIntlist *)
+(* [[[[SmlIntlistAcc *)
+let length1 il = 
+  let rec len n il =
+  match il with
+    | Nil -> n
+    | Cons (_, il) -> len (n+1) il
+  in
+  len 0 il
+(* ]]]]SmlIntlistAcc *)
 
 let () =
     let a = Cons (12, Cons (23, Nil)) in
-    Printf.printf "%d\n" (length a)
+    Printf.printf "%d\n" (length a);
+    Printf.printf "%d\n" (length1 a)
 
